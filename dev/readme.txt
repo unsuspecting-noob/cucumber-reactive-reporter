7e6ff51c353462014393389866ceb7114e1c6aa8
Nikolai Popov <nikolai.i.popov@gmail.com>
2022-09-19 13:59:32 -0700

    massaging reporter sample

---
+++ dev/readme.txt
@@ -0,0 +1,7 @@
+Some notes about development and maintainance:
+
+npm run start - runs local dev version for react development, this relies on Public folder to have _cucumber-results.json and _reporter_settings.json files to load the page.
+
+to generate _cucumber-results.json, place cucumber-results.json with cucumberjs output into public folder, then run "npm run testinstall" that will put the output into /test folder.
+
+to check in new sample to have it show up in github, check everything in /test folder into /docs. There is a magic file called ".nojekyll", its purpose is to tell github not to flatten docs structure.
--- devtest.mjs
+++ devtest.mjs
@@ -1,7 +1,7 @@
 import Reporter from "./dist/cucumber-reactive-reporter.cjs.js";
 let options = {
     "title": "Cucumber reactive reporter sample",
-    "description": "Sample cucumber tests to show off reporter features: Click on @tags to see all tags used in your report, use these to filter results using cuvumber tag expressions, ex: (@tag1 or @tag2) and not @tag5"
+    "description": "Sample cucumber tests to show off reporter features:\n Click on @tags to see all tags used in your report, use these to filter results using cucumber tag expressions, ex: (@tag1 or @tag2) and not @tag5"
 };
 let metadata = {
     "some key": "value",
